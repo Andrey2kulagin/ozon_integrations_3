b52a359d527ed1dfa8217bbb9614f39ec60c1d75
Кулагин Андрей Андреевич <s121917@guu.ru>
2023-06-02 19:37:22 +0300

    create chancellery and paper_products

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import openpyxl
+import json
 
 
 # Открываем xlsx-файл
@@ -13,67 +14,131 @@ def get_input_row(filename):
         row_data = []
         for cell in row:
             row_data.append(cell.value)
-        print(row_data)
 
 
-def get_basic_row(row):
+def get_main_image(images):
+    space_index = images.find(" ")
+    if space_index != -1:
+        return images[:space_index]
+    else:
+        if len(images) > 5:
+            return images
+        else:
+            return ""
+
+
+def get_additional_images(images):
+    space_index = images.find(" ")
+    if space_index != -1:
+        return images[space_index + 1:]
+    else:
+        return ""
+
+
+def get_basic_row(row, input_row, commercial_type):
     row.append("")
-    row.append("article")  # артикул
-    row.append("title")  # название товара
-    row.append("price")
-    row.append("price_before_sale")
-    row.append("НДС")
+    row.append(input_row[0])  # артикул
+    row.append(input_row[8])  # название товара
+    row.append(input_row[5])
+    row.append(input_row[6])
+    row.append("Не облагается")
     row.append("")
-    row.append("Commercial_type")
-    row.append("barcode")
-    row.append("weight")
-    row.append("width")
-    row.append("height")
-    row.append("length")
-    row.append("main_photo")
-    row.append("additional_photos")
+    row.append(commercial_type)
+    row.append("")
+    row.append(input_row[1])
+    row.append(input_row[3])
+    row.append(input_row[4])
+    row.append(input_row[2])
+    row.append(get_main_image(input_row[10]))
+    row.append(get_additional_images(input_row[10]))
 
 
-def get_stationery_row(input_row):
+def get_count_in_pack(input_row):
+    input_count = input_row[12]
+    if input_count == "" or not input_count:
+        return "1"
+    else:
+        return input_count
+
+
+def get_color_name(input_row):
+    return input_row[13]
+
+
+def get_brand(input_row):
+    return input_row[11]
+
+
+def get_title(input_row):
+    return input_row[8]
+
+
+def get_type(commercial_type):
+    categories_match_file = open("types.json", 'r', encoding='utf-8')
+    categories_match_dict = json.load(categories_match_file)
+    return categories_match_dict.get(commercial_type, "")
+
+
+def get_description(input_row):
+    return input_row[9]
+
+
+def get_stationery_row(input_row, commercial_type):
     """ Генерирует строку для записи в Канцелярские товары > Канцелярия """
     row = []
-    get_basic_row(row)
+    get_basic_row(row, input_row, commercial_type)
     row.extend(["", ""])
-    row.append("brand")
-    row.append("title")
-    row.append("count_in_pack")
-    row.append("color")
-    row.append("color_name")
-    row.append("color_name")
-    row.append("type")
-    row.append("produced country")
+    row.append(get_brand(input_row))
+    row.append(get_title(input_row))
+    row.append(get_count_in_pack(input_row))
+    row.append("")
+    row.append(get_color_name(input_row))
+    row.append(get_type(commercial_type))
+    row.append("")
     row.extend(['', '', '', ''])
-    row.append("description")
+    row.append(get_description(input_row))
     row.extend(['', ''])
-    row.append("material")
+    row.append("")
     return row
 
 
-def get_paper_products_row():
+def get_sheets_count(input_row):
+    sheets_count = input_row[14]
+    if sheets_count == "" or not sheets_count:
+        return '100'
+    else:
+        return sheets_count
+
+
+def get_density(input_row):
+    density = input_row[15]
+    if density == "" or not density:
+        return '60'
+    else:
+        return density
+
+
+def get_paper_products_row(input_row, commercial_type):
     """ Генерирует строку для записи в  Канцелярские товары > Бумажная продукция """
     row = []
-    get_basic_row(row)
+    get_basic_row(row, input_row, commercial_type)
     row.extend(["", ""])
-    row.append("brand")
-    row.append("title")
-    row.append("sizes_string")
-    row.append("sheets count")
-    row.append("color")
-    row.append("color_name")
-    row.append("type")
+    row.append(get_brand(input_row))
+    row.append(get_title(input_row))
     row.append("")
-    row.append("produced country")
+    row.append(get_sheets_count(input_row))
+    row.append("")
+    row.append(get_color_name(input_row))
+    row.append(get_type(commercial_type))
+    row.append("")
+    row.append("")
     row.extend(['', '', '', ''])
-    row.append("description")
+    row.append(get_description(input_row))
     row.append("")
-    row.append("markup")  # разметка бумаги(в клетку/линейку или без нее)
     row.append("")
-    row.append("density")  # плотность бумаги
+    row.append("")
+    row.append(get_density(input_row))  # плотность бумаги
+    return row
 
 
 def get_folders_files_row():
@@ -576,6 +641,7 @@ def get_set_for_creativity_row():
     row.extend(['', '', '', '', '', ''])
     row.append("description")
 
+
 def get_bag_row():
     """ Генерирует строку для записи в  Электроника > Рюкзаки, чехлы, сумки """
     row = []
@@ -589,3 +655,58 @@ def get_bag_row():
     row.append("produced country")
     row.extend(['', ''])
     row.append("description")
+
+
+def get_first_dict_value(dict):
+    if type(dict) == str:
+        return 1
+    for i in dict:
+        if get_first_dict_value(dict[i]) == 1:
+            return dict[i]
+
+
+def get_ozon_category(initial_category: str):
+    categories_match_file = open("categories.json", 'r', encoding='utf-8')
+    categories_match_dict = json.load(categories_match_file)
+    categories_match_file.close()
+    initial_category = initial_category.split("/")
+    our_category = categories_match_dict
+    for category in initial_category:
+        if category in our_category:
+            our_category = our_category[category]
+        else:
+            return False
+    if type(our_category) == str:
+        return our_category
+    elif type(our_category) == dict:
+        return get_first_dict_value(our_category)
+
+
+def write_row(filename, row):
+    wb = openpyxl.load_workbook(filename)
+    ws = wb.active
+    ws.append(row)
+    wb.save(filename)
+
+
+def main():
+    wb = openpyxl.load_workbook("input.xlsx")
+    # Получаем активный лист
+    sheet = wb.active
+    # Читаем и выводим каждую строку
+    chancellery_categories = [i.strip() for i in open("chancellery.txt", 'r', encoding='utf-8').readlines()]
+    paper_products_categories = [i.strip() for i in
+                                 open("paper_products_categories.txt", 'r', encoding='utf-8').readlines()]
+    for input_row in sheet.iter_rows(values_only=True):
+        print(input_row)
+        ozon_category = get_ozon_category(input_row[7])
+        if ozon_category in chancellery_categories:
+            row = get_stationery_row(input_row, ozon_category)
+            write_row("chancellery_output.xlsx", row)
+        if ozon_category in paper_products_categories:
+            row = get_paper_products_row(input_row, ozon_category)
+            write_row("paper_products_output.xlsx", row)
+
+
+if __name__ == "__main__":
+    main()
